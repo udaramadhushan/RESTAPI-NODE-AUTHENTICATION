5869b40974acdaa7b87fafa44e0a7f025713dd15
Udara Madhushan <udaram188@gmail.com>
2021-01-01 14:17:56 +0530

    user objects pushed to database

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 
+//import routes
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 //connect.db
-mongoose.connect(process.env.DB_CONNECT,
+mongoose.connect(process.env.DB_CONNECTION,
 { useNewUrlParser: true },
 console.log("connected to DB"));
 
 
 
+//Middleware
+app.use(express.json());
 
 
-//import routes
-const authRoute = require('./routes/auth');
 
 
 //Route middleware
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
+const User = require('../models/user');
 
-router.post('/register', (req,res)=>{
 
-    res.send('Register');
+
+router.post('/register', async(req,res)=>{
+
+    const user =  new User({
+
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+
+            const savedUser = await user.save();
+            res.send(savedUser);
+}
+    catch(err){
+        res.status(400).send(err);
+}
 })
 
 
