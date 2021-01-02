ad3fae537ffe7cdc452f27b43359b34e5d8c81de
Udara Madhushan <udaram188@gmail.com>
2021-01-02 20:38:58 +0530

    checking if email exists

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,23 @@
 const router = require('express').Router();
-const User = require('../models/user');
+const User = require('../models/User');
+const {registerValidation, loginValidation} = require("../validation");
 
 
-//VALIDATION
-const Joi = require("@hapi/joi");
 
-const schema = {
-
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6)
-}
 
 
 router.post('/register', async(req,res)=>{
 
-    //validation of data before making a user
-    const {error} = Joi.validate(req.body, schema);
+    //validation of the request
+    const {error} = registerValidation(req.body);
 
     if(error) return res.status(400).send(error.details[0].message);
 
-    
+    //check if user is already in the DB
+    const emailExist = await  User.findOne({email:req.body.email});
+    if(emailExist) return res.status(400).send('email already exists')
+
+
     const user =  new User({
 
         name: req.body.name,
